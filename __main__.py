e290c2b5b83566ae8af7ee1dde03b0bf81f31801
vivax3794 <51753506+vivax3794@users.noreply.github.com>
2019-10-01 04:40:28 +0200

    added bot

--- __main__.py
+++ __main__.py
@@ -13,5 +13,4 @@ for cog in constants.cogs:
         exc = '{}: {}'.format(type(e).__name__, e)
         print(f"Failed to load extension {cog}\n{exc}")
 
-print(constants.token)
 bot.run(constants.token)
--- constants.py
+++ constants.py
@@ -6,4 +6,8 @@ prefix = "!"
 token = os.getenv("TOKEN")
 
 main_command_channel = int(os.getenv("command_channel"))
+
+print(token)
+print(main_command_channel)
+
 command_channels = [main_command_channel]
